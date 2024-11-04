1b64aeb26aa848e10357cc186a17ba54a1bcf3d4
Aces45 <stevenignacio45@yahoo.com>
2024-11-04 20:53:45 +0800

    removed debug=True from app.run()

--- Gacha.py
+++ Gacha.py
@@ -62,7 +62,7 @@ def result():
     return render_template('result.html', displayResult = displayResult)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run()
 
 # #iterator
 # counter = 0
